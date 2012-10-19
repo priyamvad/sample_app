dbf43d5792fdc7333544024b080abd2836860cd0
Priyamvad <priyamvad@gmail.com>
2012-10-18 23:39:44 -0700

    css

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,24 @@
 require 'spec_helper'
 
+
 describe "Static Pages" do
   describe "Home page" do
+    before { visit '/static_pages/home' }
+
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App') 
     end
     
     it "should have the right title" do
-      visit '/static_pages/home'
       page.should have_selector('title',
                                 :text => " | Home") 
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit 'static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
     end
   end
 
